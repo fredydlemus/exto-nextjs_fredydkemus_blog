28a28c29486c3d8dcd8d58aa170027ce2c8f9ea4
fredydlemus <fredyflemus@gmail.com>
2022-05-16 23:25:08 -0600

    add google analytics script

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -33,6 +33,18 @@ export default class MyDocument extends Document {
         return (
             <Html {...this.helmetHtmlAttrComponents}>
                 <Head>{this.helmetHeadComponents}</Head>
+
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-KYK5FHL0DD"></script>
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+  
+    gtag('config', 'G-KYK5FHL0DD');`
+                    }}
+                />
+
                 <body {...this.helmetBodyAttrComponents}>
                     <Main />
                     <script src={withPrefix('js/plugins.js')} />
